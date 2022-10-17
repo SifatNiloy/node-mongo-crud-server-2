6c3767357af820a1760e7b04025858f14876b286
unknown <sifatniloy18@gmail.com>
2022-10-16 21:54:36 -0600

    node mongo server for practise

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
+const uri = "mongodb+srv://mydbuser2:InNhWUsbPzgJo08w@cluster0.e5psqp5.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("foodMaster2");
+        const usersCollection = database.collection("users");
+
+        //get api
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await usersCollection.findOne(query);
+            console.log('load users with id', id)
+            res.send(user)
+        })
+
+        //post api
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            console.log('got new user', req.body);
+            console.log('added user', result);
+            res.json(result);
+        })
+
+        //delete api
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query)
+            console.log('deleting user with id', result);
+            res.json(result)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('running my crud server');
+})
+app.listen(port, () => {
+    console.log('running server on port', port);
+})
